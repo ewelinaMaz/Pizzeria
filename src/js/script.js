65490958647361a59ddd6448667160887fb8af3f
Ewelina Mazurek <ewelina.laszczak7@gmail.com>
2020-01-07 16:01:10 +0100

    8.6 Image display corrected

--- src/js/script.js
+++ src/js/script.js
@@ -81,6 +81,7 @@
 
       /* add element to menu */
       menuContainer.appendChild(thisProduct.element);
+      console.log('thisProduct.element:', thisProduct.element);
     }
 
     getElements(){
@@ -109,7 +110,8 @@
         /* toggle active class on element of thisProduct */
         thisProduct.element.classList.toggle('active');
         /* find all active products */
-        const activeProducts = document.querySelectorAll('.active');
+        const activeProducts = document.querySelectorAll('.product');
+
         /* START LOOP: for each active product */
         for(let activeProduct of activeProducts){
           /* START: if the active product isn't the element of thisProduct */
